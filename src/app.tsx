537b8d3d991ab34d4022506ca89207e951cc1565
رأفت عمر <shamwebs1@gmail.com>
2025-08-15 13:43:27 +0300

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import KeynovaLock from "./components/KeynovaLock";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <KeynovaLock />
+    </div>
+  );
+}
+
+export default App;
